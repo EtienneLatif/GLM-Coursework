9f3ecab4dffcf3dbdc3754e68cd35875883a9424
Etienne <93058735+EtienneLatif@users.noreply.github.com>
2022-10-27 14:16:41 +0100

    Add files via upload

---
+++ code-31045499.R
@@ -0,0 +1,47 @@
+## R template from STAT6083 Assignment 2022-23 ##
+
+## Task 1 ##
+
+# Clear R workspace
+rm(list=ls())
+
+# Set the working directory to read the data for Task 1
+setwd("...")
+
+# Read and attach the data for Task 1
+task1.data <- read.table("expenditure.txt", header=T)
+attach(task1.data)
+
+# Below is where you should write your R code for Task 1
+# Begin each segment of code with a comment identifying the question being answered
+
+## Task 2 ##
+
+# Clear R workspace
+rm(list=ls())
+
+# Set the working directory to read the data for Task 2
+setwd("...")
+
+# Read and attach the data for Task 2
+task2.data <- read.table("airline.txt", header=T)
+attach(task2.data)
+
+# Below is where you should write your R code for Task 2
+# Begin each segment of code with a comment identifying the question being answered
+
+## Task 3 ##
+
+# Clear R workspace
+rm(list=ls())
+detach(task1.data)
+
+# Set the working directory to read the data for Task 3
+setwd("...")
+
+# Read and attach the data for Task 2
+task3.data <- read.table("expenditure.txt", header=T)
+attach(task3.data)
+ 
+# Below is where you should write your R code for Task 3
+# Begin each segment of code with a comment identifying the question being answered
